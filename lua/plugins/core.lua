3b40f476bc41db32c6cfbfc79655927774d5ad7b
Radja Fajrul Ghufron <radjaoke18@gmail.com>
2026-07-29 20:20:19 +0700

    feat(treesitter): add context plugin for scrolling headers
    Integrates `nvim-treesitter-context` to display function and class headers while scrolling.
    Adds `<leader>ut` to toggle the context view.
    Includes minor formatting adjustments for Mason and Fidget plugin configurations.

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -10,19 +10,32 @@ return {
     opts = {
       ensure_installed = {
         -- JS/TS
-        "vtsls", "typescript-language-server", "vscode-eslint-language-server",
-        "tailwindcss-language-server", "prettierd",
+        "vtsls",
+        "typescript-language-server",
+        "vscode-eslint-language-server",
+        "tailwindcss-language-server",
+        "prettierd",
         -- Go
-        "gopls", "gofumpt", "goimports", "delve",
+        "gopls",
+        "gofumpt",
+        "goimports",
+        "delve",
         -- Web
-        "vscode-html-language-server", "vscode-css-language-server",
-        "sql-language-server", "dart-format", "sql-formatter",
+        "vscode-html-language-server",
+        "vscode-css-language-server",
+        "sql-language-server",
+        "dart-format",
+        "sql-formatter",
         -- C/C++
-        "clangd", "clang-format",
+        "clangd",
+        "clang-format",
         -- Kotlin / Android
-        "kotlin-lsp", "kotlin-debug-adapter", "ktfmt",
+        "kotlin-lsp",
+        "kotlin-debug-adapter",
+        "ktfmt",
         -- Debug
-        "js-debug-adapter", "codelldb",
+        "js-debug-adapter",
+        "codelldb",
       },
     },
   },
@@ -35,7 +48,6 @@ return {
       require("conform").setup(require "configs.conform")
     end,
   },
-
   {
     "neovim/nvim-lspconfig",
     lazy = true,
@@ -47,15 +59,33 @@ return {
   },
 
   -- LSP Progress Indicator
-  {
-    "j-hui/fidget.nvim",
-    event = "LspAttach",
-  },
+  { "j-hui/fidget.nvim", event = "LspAttach" },
 
   -- Treesitter. Run :TSInstall <lang> for missing parsers; `:TSUpdate` runs on plugin update.
   {
     "nvim-treesitter/nvim-treesitter",
     event = { "BufReadPre", "BufNewFile" },
     build = ":TSUpdate",
+  },
+
+  -- Show the current function/class header while scrolling.
+  {
+    "nvim-treesitter/nvim-treesitter-context",
+    event = { "BufReadPost", "BufNewFile" },
+    cmd = { "TSContext", "TSContextEnable", "TSContextDisable", "TSContextToggle" },
+    keys = {
+      { "<leader>ut", "<cmd>TSContextToggle<cr>", desc = "Toggle Treesitter Context" },
+    },
+    opts = {
+      -- Keep the overlay small: enough context without stealing editor space.
+      max_lines = 3,
+      min_window_height = 20,
+      multiline_threshold = 3,
+      mode = "cursor",
+      trim_scope = "outer",
+      line_numbers = false,
+      multiwindow = false,
+      separator = nil,
+    },
   },
 }
